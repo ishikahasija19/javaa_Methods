3f13fe36fe765fa735382f03becea09cbfb81512
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-13 22:09:57 +0530

    RandomArrayStats.java

--- level2/RandomArrayStats.java
+++ level2/RandomArrayStats.java
@@ -4,7 +4,7 @@ public class RandomArrayStats {
     public static int[] generate4DigitRandomArray(int size) {
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
-            arr[i] = (int)(Math.random() * 9000) + 1000; // 1000 to 9999
+            arr[i] = (int)(Math.random() * 9000) + 1000; 
         }
         return arr;
     }
