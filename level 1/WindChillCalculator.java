5cf705d144f477b074a325498e48fa471e7146e8
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-13 22:06:02 +0530

    WindChillCalculator.java

--- level 1/WindChillCalculator.java
+++ level 1/WindChillCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class WindChillCalculator {
-    // Method to calculate wind chill temperature
+   
     public static double calculateWindChill(double temperature, double windSpeed) {
         return 35.74 + 0.6215 * temperature + (0.4275 * temperature - 35.75) * Math.pow(windSpeed, 0.16);
     }
